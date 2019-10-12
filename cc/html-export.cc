c5c4bf299979e5e92b6c8a4224299227076e35b6
Eugene Skepner <github@skepner.eu>
2019-10-12 08:45:16 +0200

    html export

--- cc/html-export.cc
+++ cc/html-export.cc
@@ -3,6 +3,8 @@
 #include "acmacs-tal/tree.hh"
 #include "acmacs-tal/tree-iterate.hh"
 
+// ----------------------------------------------------------------------
+
 namespace
 {
     extern const char* sHeader;
@@ -10,6 +12,11 @@ namespace
     extern const char* sRowParentNode;
     extern const char* sRowLeaf;
 } // namespace
+
+static void add_nodes_old(fmt::memory_buffer& html, const acmacs::tal::v3::Node& tree, double edge_scale);
+
+using prefix_t = std::vector<std::string>;
+static void add_nodes(fmt::memory_buffer& html, const acmacs::tal::v3::Node& node, double edge_scale, prefix_t& prefix);
 
 // ----------------------------------------------------------------------
 
@@ -21,37 +28,53 @@ std::string acmacs::tal::v3::html_export(const Tree& tree)
 
     fmt::memory_buffer html;
     fmt::format_to(html, sHeader, fmt::arg("title", fmt::format("{} {}", tree.virus_type(), tree.lineage())));
-
-    EdgeLength parent_cumulative{0.0};
-
-    const auto node_pre = [&html, &parent_cumulative, edge_scale](const auto& node) {
-        // fmt::print(stderr, "DEBUG: + {} {}\n", node.subtree.size(), node.cumulative_edge_length);
-        fmt::format_to(
-            html, sRowParentNode,
-            fmt::arg("parent_cumulative", static_cast<int>(parent_cumulative.as_number() * edge_scale)),
-            fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)));
-        parent_cumulative = node.cumulative_edge_length;
-    };
-
-    const auto node_post = [&parent_cumulative](const auto& node) {
-        parent_cumulative -= node.edge_length;
-    };
-
-    const auto leaf = [&html, &parent_cumulative, edge_scale](const auto& node) {
-        // fmt::print(stderr, "DEBUG: {} {}\n", node.seq_id, node.cumulative_edge_length);
-        fmt::format_to(
-            html, sRowLeaf,
-            fmt::arg("seq_id", node.seq_id),
-            fmt::arg("parent_cumulative", static_cast<int>(parent_cumulative.as_number() * edge_scale)),
-            fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)));
-    };
-
-    tree::iterate_leaf_pre_post(tree, leaf, node_pre, node_post);
-
+    prefix_t prefix;
+    add_nodes(html, tree, edge_scale, prefix);
     fmt::format_to(html, sFooter);
     return fmt::to_string(html);
 
 } // acmacs::tal::v3::html_export
+
+// ----------------------------------------------------------------------
+
+void add_nodes(fmt::memory_buffer& html, const acmacs::tal::v3::Node& node, double edge_scale, prefix_t& prefix)
+{
+    if (node.is_leaf()) {
+        fmt::format_to(html, "<li><table><tr>{prefix}<td><div class='leaf-edge' style='width: {edge}px;'></div></td><td class='seq-name'>{seq_id}</td></tr></table></li>",
+                       fmt::arg("prefix", ::string::join("", prefix)), fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)), fmt::arg("seq_id", node.seq_id));
+    }
+    else {
+
+        for (auto& subnode : node.subtree)
+            add_nodes(html, subnode, edge_scale, prefix);
+    }
+
+} // add_nodes
+
+// ----------------------------------------------------------------------
+
+void add_nodes_old(fmt::memory_buffer& html, const acmacs::tal::v3::Node& tree, double edge_scale)
+{
+    acmacs::tal::v3::EdgeLength parent_cumulative{0.0};
+
+    const auto node_pre = [&html, &parent_cumulative, edge_scale](const auto& node) {
+        // fmt::print(stderr, "DEBUG: + {} {}\n", node.subtree.size(), node.cumulative_edge_length);
+        fmt::format_to(html, sRowParentNode, fmt::arg("parent_cumulative", static_cast<int>(parent_cumulative.as_number() * edge_scale)),
+                       fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)));
+        parent_cumulative = node.cumulative_edge_length;
+    };
+
+    const auto node_post = [&parent_cumulative](const auto& node) { parent_cumulative -= node.edge_length; };
+
+    const auto leaf = [&html, &parent_cumulative, edge_scale](const auto& node) {
+        // fmt::print(stderr, "DEBUG: {} {}\n", node.seq_id, node.cumulative_edge_length);
+        fmt::format_to(html, sRowLeaf, fmt::arg("seq_id", node.seq_id), fmt::arg("parent_cumulative", static_cast<int>(parent_cumulative.as_number() * edge_scale)),
+                       fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)));
+    };
+
+    acmacs::tal::v3::tree::iterate_leaf_pre_post(tree, leaf, node_pre, node_post);
+
+} // add_nodes
 
 // ----------------------------------------------------------------------
 
@@ -65,7 +88,7 @@ namespace
   <style>
    ul.tree {{ list-style: none; margin: 0; padding: 0; }}
    ul.tree > li {{ white-space: nowrap; }}
-   .parent-cumulative {{ min-height: 1px; }}
+   .parent-cumulative {{  }}
    .leaf-edge {{ height: 0.5em; border-bottom: 1px solid black; border-left: 1px solid black; }}
    .seq-name {{}}
    div.parent-cumulative {{ float: left; min-height: 1px; }}
@@ -106,14 +129,6 @@ namespace
       </tr>
      </table>
     </li>
-)";
-
-    const char* sRowParentNode_divs = R"(
-    <li><div class='parent-cumulative' style='width: {parent_cumulative}px;'></div><div class='leaf-edge' style='width: {edge}px;'></div><div class='seq-name'></div></li>
-)";
-
-    const char* sRowLeaf_divs = R"(
-   <li><div class='parent-cumulative' style='width: {parent_cumulative}px;'></div><div class='leaf-edge' style='width: {edge}px;'></div><div class='seq-name'>{seq_id}</div></li>
 )";
 
 } // namespace
