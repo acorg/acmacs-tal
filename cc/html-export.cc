61c188bf1a8eada671b767df5d3746dc64fcb5a5
Eugene Skepner <github@skepner.eu>
2019-10-14 11:49:58 +0200

    html export

--- cc/html-export.cc
+++ cc/html-export.cc
@@ -1,4 +1,5 @@
 #include "acmacs-base/fmt.hh"
+#include "acmacs-base/string.hh"
 #include "acmacs-tal/html-export.hh"
 #include "acmacs-tal/tree.hh"
 #include "acmacs-tal/tree-iterate.hh"
@@ -9,14 +10,10 @@ namespace
 {
     extern const char* sHeader;
     extern const char* sFooter;
-    extern const char* sRowParentNode;
-    extern const char* sRowLeaf;
 } // namespace
 
-static void add_nodes_old(fmt::memory_buffer& html, const acmacs::tal::v3::Node& tree, double edge_scale);
-
 using prefix_t = std::vector<std::string>;
-static void add_nodes(fmt::memory_buffer& html, const acmacs::tal::v3::Node& node, double edge_scale, prefix_t& prefix);
+static void add_nodes(fmt::memory_buffer& html, const acmacs::tal::v3::Node& node, double edge_scale, prefix_t& prefix, bool last);
 
 // ----------------------------------------------------------------------
 
@@ -29,7 +26,7 @@ std::string acmacs::tal::v3::html_export(const Tree& tree)
     fmt::memory_buffer html;
     fmt::format_to(html, sHeader, fmt::arg("title", fmt::format("{} {}", tree.virus_type(), tree.lineage())));
     prefix_t prefix;
-    add_nodes(html, tree, edge_scale, prefix);
+    add_nodes(html, tree, edge_scale, prefix, false);
     fmt::format_to(html, sFooter);
     return fmt::to_string(html);
 
@@ -37,42 +34,28 @@ std::string acmacs::tal::v3::html_export(const Tree& tree)
 
 // ----------------------------------------------------------------------
 
-void add_nodes(fmt::memory_buffer& html, const acmacs::tal::v3::Node& node, double edge_scale, prefix_t& prefix)
+void add_nodes(fmt::memory_buffer& html, const acmacs::tal::v3::Node& node, double edge_scale, prefix_t& prefix, bool last)
 {
     if (node.is_leaf()) {
-        fmt::format_to(html, "<li><table><tr>{prefix}<td><div class='leaf-edge' style='width: {edge}px;'></div></td><td class='seq-name'>{seq_id}</td></tr></table></li>",
-                       fmt::arg("prefix", ::string::join("", prefix)), fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)), fmt::arg("seq_id", node.seq_id));
+        fmt::format_to(html, "<li><table><tr>{prefix}<td><div class='node-edge {node_edge_last}' style='width: {edge}px;'></div></td><td class='seq-name'>{seq_id}</td></tr></table></li>",
+                       fmt::arg("prefix", ::string::join("", prefix)), fmt::arg("node_edge_last", last ? "node-edge-last" : ""),
+                       fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)), fmt::arg("seq_id", node.seq_id));
     }
     else {
-
-        for (auto& subnode : node.subtree)
-            add_nodes(html, subnode, edge_scale, prefix);
+        const auto edge = static_cast<int>(node.edge_length.as_number() * edge_scale);
+        fmt::format_to(html, "<li><table><tr>{prefix}<td><div class='node-edge {node_edge_last}' style='width: {edge}px;'></div></td></tr></table></li>",
+                       fmt::arg("prefix", ::string::join("", prefix)), fmt::arg("node_edge_last", last ? "node-edge-last" : ""), fmt::arg("edge", edge));
+        prefix.push_back(fmt::format("<td class='subnode subnode-middle'><div style='width: {edge}px;'></div></td>", fmt::arg("edge", edge)));
+        for (auto subnode = std::begin(node.subtree); subnode != std::end(node.subtree); ++subnode) {
+            if (!subnode->hidden) {
+                const auto sub_last = std::next(subnode) == std::end(node.subtree);
+                if (sub_last)
+                    prefix.back() = ::string::replace(prefix.back(), "subnode-middle", "subnode-last");
+                add_nodes(html, *subnode, edge_scale, prefix, sub_last);
+            }
+        }
+        prefix.pop_back();
     }
-
-} // add_nodes
-
-// ----------------------------------------------------------------------
-
-void add_nodes_old(fmt::memory_buffer& html, const acmacs::tal::v3::Node& tree, double edge_scale)
-{
-    acmacs::tal::v3::EdgeLength parent_cumulative{0.0};
-
-    const auto node_pre = [&html, &parent_cumulative, edge_scale](const auto& node) {
-        // fmt::print(stderr, "DEBUG: + {} {}\n", node.subtree.size(), node.cumulative_edge_length);
-        fmt::format_to(html, sRowParentNode, fmt::arg("parent_cumulative", static_cast<int>(parent_cumulative.as_number() * edge_scale)),
-                       fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)));
-        parent_cumulative = node.cumulative_edge_length;
-    };
-
-    const auto node_post = [&parent_cumulative](const auto& node) { parent_cumulative -= node.edge_length; };
-
-    const auto leaf = [&html, &parent_cumulative, edge_scale](const auto& node) {
-        // fmt::print(stderr, "DEBUG: {} {}\n", node.seq_id, node.cumulative_edge_length);
-        fmt::format_to(html, sRowLeaf, fmt::arg("seq_id", node.seq_id), fmt::arg("parent_cumulative", static_cast<int>(parent_cumulative.as_number() * edge_scale)),
-                       fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)));
-    };
-
-    acmacs::tal::v3::tree::iterate_leaf_pre_post(tree, leaf, node_pre, node_post);
 
 } // add_nodes
 
@@ -88,13 +71,12 @@ namespace
   <style>
    ul.tree {{ list-style: none; margin: 0; padding: 0; }}
    ul.tree > li {{ white-space: nowrap; }}
-   .parent-cumulative {{  }}
-   .leaf-edge {{ height: 0.5em; border-bottom: 1px solid black; border-left: 1px solid black; }}
-   .seq-name {{}}
-   div.parent-cumulative {{ float: left; min-height: 1px; }}
-   div.leaf-edge {{ float: left; height: 0.5em; border-bottom: 1px solid black; border-left: 1px solid black; }}
-   div.seq-name {{}}
+   ul.tree table {{ border-collapse: collapse; }}
    ul.tree td {{ padding: 0; vertical-align: top; }}
+   ul.tree td.subnode-middle {{ border-right: 1px solid black; }}
+   ul.tree td.subnode-last {{ border-right: none; }}
+   ul.tree div.node-edge {{ height: 0.5em; border-bottom: 1px solid black; }}
+   ul.tree div.node-edge-last {{ border-left: 1px solid black; }}
   </style>
  </head>
  <body>
@@ -106,29 +88,6 @@ namespace
   </ul>
  </body>
 <html>
-)";
-
-    const char* sRowParentNode = R"(
-    <li>
-     <table>
-      <tr>
-       <td class='parent-cumulative' style='width: {parent_cumulative}px;'></td>
-       <td><div class='leaf-edge' style='width: {edge}px;'></div></td>
-      </tr>
-     </table>
-    </li>
-)";
-
-    const char* sRowLeaf = R"(
-    <li>
-     <table>
-      <tr>
-       <td class='parent-cumulative' style='width: {parent_cumulative}px;'></td>
-       <td><div class='leaf-edge' style='width: {edge}px;'></div></td>
-       <td class='seq-name'>{seq_id}</td>
-      </tr>
-     </table>
-    </li>
 )";
 
 } // namespace
