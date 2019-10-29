56a9e43da42775b596da5081de09e0755a745558
Eugene Skepner <github@skepner.eu>
2019-10-29 12:47:36 +0100

    minor fix

--- cc/settings.hh
+++ cc/settings.hh
@@ -15,7 +15,7 @@ namespace acmacs::tal::inline v3
         void tree(Tree& tree);
         Tree& tree() const { if (!tree_) throw error{"tree was not set"}; return *tree_; }
 
-        virtual bool apply_built_in(std::string_view name); // returns true if built-in command with that name found and applied
+        bool apply_built_in(std::string_view name) override; // returns true if built-in command with that name found and applied
 
         void report_nodes(std::string_view prefix, std::string_view indent, const NodeSet& nodes) const;
         NodeSet select_nodes(const rjson::value& criteria) const;
