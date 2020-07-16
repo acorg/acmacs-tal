082b4a91ffaba878f674ed465b5fd4e1fe04721c
Eugene Skepner <github@skepner.eu>
2020-07-16 18:50:12 +0200

    generate compacter html

--- cc/html-export.cc
+++ cc/html-export.cc
@@ -50,29 +50,29 @@ void add_nodes(fmt::memory_buffer& html, const acmacs::tal::v3::Node& node, cons
 {
     if (node.is_leaf()) {
         fmt::format_to(html,
-                       "<li><table><tr>{prefix}<td><div class='node-edge {node_edge_last}' style='width: {edge}px;'></div></td><td class='seq-name' style='color: "
+                       "<li><table><tr>{prefix}<td><div class='e {node_edge_last}' style='width: {edge}px;'></div></td><td class='seq-name' style='color: "
                        "{color_tree_label}'>{seq_id}</td></tr></table></li>\n",
-                       fmt::arg("prefix", acmacs::string::join(acmacs::string::join_concat, prefix)), fmt::arg("node_edge_last", last ? "node-edge-last" : ""),
+                       fmt::arg("prefix", acmacs::string::join(acmacs::string::join_concat, prefix)), fmt::arg("node_edge_last", last ? "n" : ""),
                        fmt::arg("edge", static_cast<int>(node.edge_length.as_number() * edge_scale)), fmt::arg("seq_id", node.seq_id),
                        fmt::arg("color_tree_label", "black" /*node.color_tree_label.to_hex_string()*/));
     }
     else {
         const auto edge = static_cast<int>(node.edge_length.as_number() * edge_scale);
-        fmt::format_to(html, "<li><table><tr>{prefix}<td><div class='node-edge {node_edge_last}' style='width: {edge}px;'></div></td>", fmt::arg("prefix", acmacs::string::join(acmacs::string::join_concat, prefix)),
-                       fmt::arg("node_edge_last", last ? "node-edge-last" : ""), fmt::arg("edge", edge));
+        fmt::format_to(html, "<li><table><tr>{prefix}<td><div class='e {node_edge_last}' style='width: {edge}px;'></div></td>", fmt::arg("prefix", acmacs::string::join(acmacs::string::join_concat, prefix)),
+                       fmt::arg("node_edge_last", last ? "n" : ""), fmt::arg("edge", edge));
         // if (node.number_leaves_in_subtree() >= 20) {
             // if (const auto rep = node.common_aa_.report(parent.common_aa_); !rep.empty())
-            //     fmt::format_to(html, "<td class='common-aa'>leaves:{} {}</td>", node.number_leaves_in_subtree(), rep);
+            //     fmt::format_to(html, "<td class='a'>leaves:{} {}</td>", node.number_leaves_in_subtree(), rep);
         if (const auto rep = node.aa_transitions_.display(); !rep.empty())
-            fmt::format_to(html, "<td class='common-aa'>{}leaves:{} {} -- left:{}</td>", node.seq_id.empty() ? std::string{} : fmt::format("[{}] ", node.seq_id), node.number_leaves_in_subtree(), rep, node.node_for_left_aa_transitions_ ? node.node_for_left_aa_transitions_->seq_id : std::string_view{});
+            fmt::format_to(html, "<td class='a'>{}leaves:{} {} -- left:{}</td>", node.seq_id.empty() ? std::string{} : fmt::format("[{}] ", node.seq_id), node.number_leaves_in_subtree(), rep, node.node_for_left_aa_transitions_ ? node.node_for_left_aa_transitions_->seq_id : std::string_view{});
         // }
         fmt::format_to(html, "</tr></table></li>\n");
-        prefix.push_back(fmt::format("<td class='subnode subnode-middle'><div style='width: {edge}px;'></div></td>", fmt::arg("edge", edge)));
+        prefix.push_back(fmt::format("<td class='subnode m'><div style='width: {edge}px;'></div></td>", fmt::arg("edge", edge)));
         for (auto subnode = std::begin(node.subtree); subnode != std::end(node.subtree); ++subnode) {
             if (subnode->children_are_shown()) {
                 const auto sub_last = std::next(subnode) == std::end(node.subtree);
                 if (sub_last)
-                    prefix.back() = ::string::replace(prefix.back(), "subnode-middle", "subnode-last");
+                    prefix.back() = ::string::replace(prefix.back(), "m", "l");
                 add_nodes(html, *subnode, node, edge_scale, prefix, sub_last);
             }
         }
@@ -95,11 +95,11 @@ namespace
    ul.tree > li {{ white-space: nowrap; }}
    ul.tree table {{ border-collapse: collapse; }}
    ul.tree td {{ padding: 0; vertical-align: top; }}
-   ul.tree td.subnode-middle {{ border-right: 1px solid black; }}
-   ul.tree td.subnode-last {{ border-right: none; }}
-   ul.tree td.common-aa {{ color: blue; font-size: 0.8em; }}
-   ul.tree div.node-edge {{ height: 0.5em; border-bottom: 1px solid black; }}
-   ul.tree div.node-edge-last {{ border-left: 1px solid black; }}
+   ul.tree td.m {{ border-right: 1px solid black; }}                  /* subnode-middle */
+   ul.tree td.l {{ border-right: none; }}                             /* subnode-last */
+   ul.tree td.a {{ color: blue; font-size: 0.8em; }}                  /* common-aa */
+   ul.tree div.e {{ height: 0.5em; border-bottom: 1px solid black; }} /* node-edge */
+   ul.tree div.n {{ border-left: 1px solid black; }}                  /* node-edge-last */
   </style>
  </head>
  <body>
