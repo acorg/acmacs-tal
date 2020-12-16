17d6d296118549f42c9c2f74cec59300add1065c
Eugene Skepner <github@skepner.eu>
2020-12-16 09:10:05 +0100

    g++-10

--- cc/settings.cc
+++ cc/settings.cc
@@ -1208,9 +1208,10 @@ void acmacs::tal::v3::Settings::add_draw_aa_transitions()
             aa_transitions.report_pos = debug_pos.to<seqdb::pos1_t>();
         if (const auto& method_v = getenv("method"sv); !method_v.is_null()) {
             const auto method = method_v.to<std::string_view>();
-            if (method.empty())
-                ; // default is in draw-tree.hh
-            if (method == "eu_20200909"sv || method == "eu-20200909"sv)
+            if (method.empty()) {
+                // empty, default is in draw-tree.hh
+            }
+            else if (method == "eu_20200909"sv || method == "eu-20200909"sv)
                 aa_transitions.method = draw_tree::AATransitionsParameters::method::eu_20200909;
             else if (method == "eu_20200915"sv || method == "eu-20200915"sv)
                 aa_transitions.method = draw_tree::AATransitionsParameters::method::eu_20200915;
