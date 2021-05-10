38d865640df23fb606099fdbc18709a441877d63
Eugene Skepner <github@skepner.eu>
2021-05-10 17:52:20 +0200

    bug fix

--- cc/newick.cc
+++ cc/newick.cc
@@ -54,6 +54,8 @@ class Tokenizer
 
     std::string_view rest() const { return data_; }
 
+    size_t pos() const { return static_cast<size_t>(data_.data() - data_all_.data()); }
+
   private:
     std::string_view data_;
     std::string_view data_all_;
@@ -82,7 +84,7 @@ class Tokenizer
                 return Token{name, edge};
             }
             default:
-                throw TokenizerError(fmt::format("unexpected symbol '{}' at pos {}", data_.front(), data_.data() - data_all_.data()));
+                throw TokenizerError(fmt::format("unexpected symbol '{}' at pos {}", data_.front(), pos()));
         }
     }
 
@@ -139,6 +141,8 @@ void acmacs::tal::v3::newick_import(std::string_view filename, Tree& tree)
                         node_stack.push(&node_stack.top()->add_subtree());
                     break;
                 case Tokenizer::Leaf:
+                    if (token.name.empty())
+                        AD_WARNING("empty leaf name at {}", tokenizer.pos());
                     // AD_DEBUG("Leaf {} :{} -- {}", token.name, token.edge_length, tokenizer.rest().substr(0, 20));
                     node_stack.top()->add_leaf(seq_id_t{token.name}, EdgeLength{token.edge_length});
                     ++leaves;
@@ -166,51 +170,47 @@ void acmacs::tal::v3::newick_import(std::string_view filename, Tree& tree)
 
 namespace acmacs::tal::inline v3
 {
-    static std::string export_node(const acmacs::tal::v3::Node& node, const ExportOptions& options);
-}
+    // returns if comma expected to be appended
+    inline bool export_node(fmt::memory_buffer& out, const acmacs::tal::v3::Node& node, const ExportOptions& options, bool prepend_comma)
+    {
+        if (node.hidden)
+            return prepend_comma;
 
-std::string acmacs::tal::v3::newick_export(const Tree& tree, const ExportOptions& options)
-{
-    std::string result = export_node(tree, options);
-    result.append(1, ';');
-    return result;
+        if (prepend_comma)
+            fmt::format_to(out, ",");
 
-} // acmacs::tal::v3::newick_export
-
-// ----------------------------------------------------------------------
-
-std::string acmacs::tal::v3::export_node(const acmacs::tal::v3::Node& node, const ExportOptions& options)
-{
-    std::string result;
-    if (!node.hidden) {
         if (node.is_leaf()) {
-            result.append(node.seq_id);
+            fmt::format_to(out, "{}", node.seq_id);
         }
         else {
-            result.append(1, '(');
-            for (const auto& sub_node : node.subtree) {
-                if (result.back() != '(')
-                    result.append(1, ',');
-                result.append(export_node(sub_node, options));
-            }
-            result.append(1, ')');
+            fmt::format_to(out, "(");
+            bool add_comma = false;
+            for (const auto& sub_node : node.subtree)
+                add_comma = export_node(out, sub_node, options, add_comma);
+            fmt::format_to(out, ")");
             if (options.add_aa_substitution_labels && !node.aa_transitions_.empty()) {
                 auto label = node.aa_transitions_.display();
                 ::string::replace_in_place(label, ' ', '_');
-                result.append(label);
+                fmt::format_to(out, "{}", label);
             }
         }
-        if (!node.edge_length.is_zero()) {
-            result.append(1, ':');
-            result.append(node.edge_length.as_string());
-        }
+        if (!node.edge_length.is_zero())
+            fmt::format_to(out, ":{}", node.edge_length.as_string());
+        return true;
     }
-    return result;
 
-} // acmacs::tal::v3::export_node
+} // namespace acmacs::tal::inline v3
 
 // ----------------------------------------------------------------------
 
+std::string acmacs::tal::v3::newick_export(const Tree& tree, const ExportOptions& options)
+{
+    fmt::memory_buffer out;
+    export_node(out, tree, options, false);
+    fmt::format_to(out, ";");
+    return fmt::to_string(out);
+
+} // acmacs::tal::v3::newick_export
 
 // ----------------------------------------------------------------------
 /// Local Variables:
