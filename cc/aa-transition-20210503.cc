c06a771d9fe0fc553ad3954a90353737f048dc24
Eugene Skepner <github@skepner.eu>
2021-05-11 12:44:41 +0200

    debugging

--- cc/aa-transition-20210503.cc
+++ cc/aa-transition-20210503.cc
@@ -84,14 +84,14 @@ void acmacs::tal::v3::detail::update_aa_transitions_eu_20210503(Tree& tree, cons
 
     set_closest_leaf_for_intermediate(tree);
 
-    const auto find_closest_with_aa_at = [](seqdb::pos0_t pos, const Node& node) -> std::pair<char, const Node*> {
+    const auto find_closest_with_aa_at = [number_of_leaves_in_tree=tree.number_leaves_in_subtree()](seqdb::pos0_t pos, const Node& node) -> std::pair<char, const Node*> {
         auto aa{'X'};
         for (const auto* bcl : node.closest_leaves) {
             aa = bcl->aa_sequence.at(pos);
             if (aa != 'X' && aa != ' ')
                 return {aa, bcl};
         }
-        if (node.number_leaves_in_subtree() > node.closest_leaves.size() && *pos < static_cast<size_t>(static_cast<double>(*node.closest_leaves[0]->aa_sequence.size()) * 0.9)) // ignore last 10% of positions anyway
+        if (number_of_leaves_in_tree < 10000 && node.number_leaves_in_subtree() > node.closest_leaves.size() && *pos < static_cast<size_t>(static_cast<double>(*node.closest_leaves[0]->aa_sequence.size()) * 0.9)) // ignore last 10% of positions anyway
             AD_WARNING("update_aa_transitions_eu_20210503: no closest leaf sequence with certain amino acid at {} found for node:{} (num-leaves:{}, closest-leaves:{}), increase "
                        "max_number_of_closest_leaves_to_set in aa-transitions.cc:219",
                        pos, node.node_id, node.number_leaves_in_subtree(), node.closest_leaves.size());
